b205150affd767e0a42beeaf4e91879fd831532d
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-23 12:39:52 +0330

    first initialize

---
+++ src/CSharp/EasyMicroservices.TemplateGeneratorMicroservice.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyMicroservices.TemplateGeneratorMicroservice.ConsoleApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
