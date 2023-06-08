319aa157a0d070c4872af8a60e476b8f77716413
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-08 13:09:26 +0330

    update domain stuff

--- src/CSharp/EasyMicroservices.TemplateGeneratorMicroservice.Domain/DataTypes/ItemType.cs
+++ src/CSharp/EasyMicroservices.TemplateGeneratorMicroservice.Domain/DataTypes/ItemType.cs
@@ -8,5 +8,12 @@
         Other = 3,
         Unknown = 4,
         Nothing = 5,
+        TextBox = 6,
+        CheckList = 7,
+        CheckBox = 8,
+        OptionList = 9,
+        DateTime = 10,
+        DateOnly = 11,
+        TimeOnly = 12,
     }
 }
