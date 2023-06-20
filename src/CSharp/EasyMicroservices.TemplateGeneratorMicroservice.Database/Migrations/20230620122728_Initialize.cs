08c9ae461917285773373723a3c7a537288232cc
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-20 16:15:18 +0330

    add initialize database and sql stuff

---
+++ src/CSharp/EasyMicroservices.TemplateGeneratorMicroservice.Database/Migrations/20230620122728_Initialize.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyMicroservices.TemplateGeneratorMicroservice.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forms",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemTypes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormDetails",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FormId = table.Column<long>(type: "bigint", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormDetails_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormFills",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FormId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormFills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormFills_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FormId = table.Column<long>(type: "bigint", nullable: false),
+                    ItemTypeId = table.Column<long>(type: "bigint", nullable: false),
+                    ParentFormItemId = table.Column<long>(type: "bigint", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormItems_FormItems_ParentFormItemId",
+                        column: x => x.ParentFormItemId,
+                        principalTable: "FormItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FormItems_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FormItems_ItemTypes_FormId",
+                        column: x => x.FormId,
+                        principalTable: "ItemTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormItemValues",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FormFilledId = table.Column<long>(type: "bigint", nullable: false),
+                    FormItemId = table.Column<long>(type: "bigint", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormItemValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormItemValues_FormFills_FormFilledId",
+                        column: x => x.FormFilledId,
+                        principalTable: "FormFills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FormItemValues_FormItems_FormItemId",
+                        column: x => x.FormItemId,
+                        principalTable: "FormItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormDetails_FormId",
+                table: "FormDetails",
+                column: "FormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormFills_FormId",
+                table: "FormFills",
+                column: "FormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormItems_FormId",
+                table: "FormItems",
+                column: "FormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormItems_ParentFormItemId",
+                table: "FormItems",
+                column: "ParentFormItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormItemValues_FormFilledId",
+                table: "FormItemValues",
+                column: "FormFilledId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormItemValues_FormItemId",
+                table: "FormItemValues",
+                column: "FormItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemTypes_Type",
+                table: "ItemTypes",
+                column: "Type",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormDetails");
+
+            migrationBuilder.DropTable(
+                name: "FormItemValues");
+
+            migrationBuilder.DropTable(
+                name: "FormFills");
+
+            migrationBuilder.DropTable(
+                name: "FormItems");
+
+            migrationBuilder.DropTable(
+                name: "Forms");
+
+            migrationBuilder.DropTable(
+                name: "ItemTypes");
+        }
+    }
+}
