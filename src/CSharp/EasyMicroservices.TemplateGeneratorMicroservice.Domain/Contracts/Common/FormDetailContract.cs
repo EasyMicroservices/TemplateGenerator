0a9e9881707651d7a13767bc935357dc61f65c8a
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-27 12:46:14 +0330

    add form detail controller

---
+++ src/CSharp/EasyMicroservices.TemplateGeneratorMicroservice.Domain/Contracts/Common/FormDetailContract.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyMicroservices.TemplateGeneratorMicroservice.Contracts.Common
+{
+    public class FormDetailContract
+    {
+        public string Title { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public long FormId { get; set; }
+    }
+}
