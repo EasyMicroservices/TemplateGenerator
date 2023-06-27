cf48c60b1607f43b11498a6d632ef5a0a5226520
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-27 12:45:31 +0330

    add migration

---
+++ src/CSharp/EasyMicroservices.TemplateGeneratorMicroservice.Database/Migrations/20230627091305_AddProperty_Value_FormDetailEntity.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyMicroservices.TemplateGeneratorMicroservice.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertyValueFormDetailEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Index",
+                table: "FormDetails");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                table: "FormDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Value",
+                table: "FormDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "FormDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "FormDetails");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Index",
+                table: "FormDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
