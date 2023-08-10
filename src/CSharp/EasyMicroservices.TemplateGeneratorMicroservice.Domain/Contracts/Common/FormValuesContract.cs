c7ad760220b4d7eb146c9a9795a4044e23fbdeba
Ali Yousefi <ali.visual.studio@gmail.com>
2023-08-10 13:01:15 +0330

    Add id to form value

--- src/CSharp/EasyMicroservices.TemplateGeneratorMicroservice.Domain/Contracts/Common/FormValuesContract.cs
+++ src/CSharp/EasyMicroservices.TemplateGeneratorMicroservice.Domain/Contracts/Common/FormValuesContract.cs
@@ -4,6 +4,7 @@ namespace EasyMicroservices.TemplateGeneratorMicroservice.Contracts.Common
 {
     public class FormValuesContract
     {
+        public long Id { get; set; }
         public long FormId { get; set; }
         public string Name { get; set; }
         public List<FormItemValueContract> FormItemValues { get; set; }
